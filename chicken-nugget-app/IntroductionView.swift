40616d9dc1fe45781218d1f9fc25e4a5a4340097
pagejenna <jwjx8888@gmail.com>
2025-08-02 11:25:14 +0800

    added intro and loading screen view

---
+++ chicken-nugget-app/IntroductionView.swift
@@ -0,0 +1,18 @@
+//
+//  IntroductionView.swift
+//  chicken-nugget-app
+//
+//  Created by jenna on 2/8/25.
+//
+
+import SwiftUI
+
+struct IntroductionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    IntroductionView()
+}
