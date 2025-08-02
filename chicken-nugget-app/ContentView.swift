08ac0f0c9d33534f093d934cae78ef928a483301
pagejenna <jwjx8888@gmail.com>
2025-08-02 11:15:08 +0800

    Initial Commit

---
+++ chicken-nugget-app/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  chicken-nugget-app
+//
+//  Created by jenna on 2/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ chicken-nugget-app/chicken_nugget_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  chicken_nugget_appApp.swift
+//  chicken-nugget-app
+//
+//  Created by jenna on 2/8/25.
+//
+
+import SwiftUI
+
+@main
+struct chicken_nugget_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
