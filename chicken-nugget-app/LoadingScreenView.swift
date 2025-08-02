11dff6d0f33fc85b7897ef9aaaa17ac10eb16d44
daniel <danielzacchung@gmail.com>
2025-08-02 12:12:47 +0800

    added basic code

--- chicken-nugget-app/LoadingScreenView.swift
+++ chicken-nugget-app/LoadingScreenView.swift
@@ -8,14 +8,38 @@
 import SwiftUI
 
 struct LoadingScreenView: View {
+    @State var isAnimating = false
+    @State private var isRising = false
+    @State private var isRIZZLER = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack{
+            Color.yellow
+                        .ignoresSafeArea()
+            VStack {
+                
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                    
+                                    .frame(width: 200, height: 5)
+                                    .offset(y: isRising ? 0 : 50) //
+                                    .animation(.easeOut(duration: 0.5), value: isRising)
+                                    .opacity(isRising ? 1 : 0)
+                                   
+                Text("Presented by")
+                    .scaleEffect(isAnimating ? 2 : 0)
+                    .animation(.bouncy,value: isAnimating)
+                Text("chicen nuget studios")
+                    .scaleEffect(isAnimating ? 2 : 0)
+                    .animation(.bouncy,value: isAnimating)
+                Button("Start") {
+                    isAnimating.toggle()
+                    isRising.toggle()
+                }
+            }
+            .padding()
         }
-        .padding()
     }
 }
 
