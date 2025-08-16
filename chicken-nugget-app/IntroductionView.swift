053a1adf2f62601c8479fcca1b0d4f505e168c08
pagejenna <jwjx8888@gmail.com>
2025-08-16 09:58:38 +0800

    star wars loading animation

--- chicken-nugget-app/IntroductionView.swift
+++ chicken-nugget-app/IntroductionView.swift
@@ -8,7 +8,10 @@
 import SwiftUI
 
 struct IntroductionView: View {
+    private let startAnimationDuration = 5.0
+    @State private var animationStart = false
     @State private var scale = 0.5
+    @State private var offset = 10.0
     var body: some View {
         HStack{
             Spacer()
@@ -16,9 +19,14 @@ struct IntroductionView: View {
             ZStack{
                 RoundedRectangle(cornerRadius: 25)
                     .fill(Color.yellow)
-                Text("Cluckster, a curious and determined chicken who dreams of a life beyond the wire fences of the farm. Tired of the same old feed and clucking routine, Cluckster seizes a chaotic moment to make a run for it—narrowly escaping the grasp of surprised farmers and slipping through the gates to freedom. Now, out in the vast real world for the first time, Cluckster must navigate bustling cities while staying one step ahead of those who want to bring her back. Help Cluckster escape from the farmers!")
-                    .rotation3DEffect(.degrees(50), axis: (x: 1, y: 0, z: 0))
-            
+                Text("Cluckster, a curious and determined chicken who dreams of a life beyond the wire fences of the farm. \nTired of the same old feed and clucking routine, Cluckster seizes a chaotic moment to make a run for it—narrowly escaping the grasp of surprised farmers and slipping through the gates to freedom. \nNow, out in the vast real world for the first time, Cluckster must navigate bustling cities while staying one step ahead of those who want to bring her back. \nHelp Cluckster escape from the farmers!")
+                        .rotation3DEffect(.degrees(40), axis: (x: 1, y: 0, z: 0))
+                        .animation(Animation.linear(duration: startAnimationDuration))
+                        .frame(width: 400, height: animationStart ? 700 : 0)
+                        .onAppear() {
+                            self.animationStart.toggle()
+                        }
+
             }
             
                 
