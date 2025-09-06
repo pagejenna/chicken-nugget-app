b7893cb99fd62bd36b75061eb285fca731467af7
daniel <danielzacchung@gmail.com>
2025-09-06 12:30:18 +0800

    dcsf

---
+++ chicken-nugget-app/Seantest.swift
@@ -0,0 +1,18 @@
+//
+//  Seantest.swift
+//  chicken-nugget-app
+//
+//  Created by T Krobot on 6/9/25.
+//
+
+import SwiftUI
+
+struct Seantest: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Seantest()
+}
